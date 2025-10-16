32950eaf81c8b555d44d5f41e3408e432449a72c
AbeyGaha <dr.abhinav.chauhan46@gmail.com>
2025-10-16 15:23:52 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,9 @@
+# config.py
 
+COMPANY_NAME = "DevStackHQ"
+CONTACT_EMAIL = "contact@devstackhq.com"
+SMTP_SETTINGS = {
+    "server": "smtp.mailersend.net",
+    "port": 587,
+    "username": "apikey",
+    "password": "your_api_key_here"  # Load from secrets.toml
